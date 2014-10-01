107cf7bc4056ec9eb5dbdba9499a7ae8026c6557
Kyle Reczek <savethejets@hotmail.com>
2014-09-30 23:14:59 -0600

    Added Jade Templates
    + Added Jade Templates through the Jade4J library.

---
+++ spark-template-jade/src/test/java/spark/template/jade/JadeTemplateExample.java
@@ -0,0 +1,18 @@
+package spark.template.jade;
+
+import spark.ModelAndView;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
+public class JadeTemplateExample {
+    public static void main(String[] args) {
+        Map map = new HashMap();
+        map.put("name", "Godzilla");
+
+        // hello.jade file is in resources/templates directory
+        get("/hello", (rq, rs) -> new ModelAndView(map, "hello"), new JadeEngine());
+    }
+}
