0463bdb07d1849bca1a1fa77d04e1fe08826274a
Johannes Renner <jrenner@suse.de>
2015-05-19 12:15:52 +0200

    Rework the JadeTemplateEngine class internally
    - add license header
    - add javadoc comments
    - call internal constructor using this()
    - organize imports
    - add @SuppressWarnings("unchecked") to render()

--- spark-template-jade/src/main/java/spark/template/jade/JadeTemplateEngine.java
+++ spark-template-jade/src/main/java/spark/template/jade/JadeTemplateEngine.java
@@ -1,40 +1,65 @@
+/**
+ * Copyright (c) 2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spark.template.jade;
 
 import de.neuland.jade4j.JadeConfiguration;
-import de.neuland.jade4j.template.ClasspathTemplateLoader;
-import de.neuland.jade4j.template.FileTemplateLoader;
 import de.neuland.jade4j.template.JadeTemplate;
-import spark.ModelAndView;
-import spark.TemplateEngine;
-import spark.template.jade.loader.SparkClasspathLoader;
 
 import java.io.IOException;
 import java.util.Map;
 
-public class JadeEngine extends TemplateEngine {
+import spark.ModelAndView;
+import spark.TemplateEngine;
+import spark.template.jade.loader.SparkClasspathLoader;
+
+/**
+ * Template engine to render HTML from jade templates.
+ */
+public class JadeTemplateEngine extends TemplateEngine {
 
     private JadeConfiguration configuration;
 
-    public JadeEngine() {
+    /**
+     * Construct a jade template engine defaulting to the 'templates' directory
+     * under the resource path.
+     */
+    public JadeTemplateEngine() {
+        this("templates/");
+    }
+
+    /**
+     * Construct a jade template engine.
+     *
+     * @param templateRoot the template root directory to use
+     */
+    public JadeTemplateEngine(String templateRoot) {
         configuration = new JadeConfiguration();
-        configuration.setTemplateLoader(new SparkClasspathLoader("templates/"));
+        configuration.setTemplateLoader(new SparkClasspathLoader(templateRoot));
     }
 
-    public JadeEngine(String contentRoot) {
-        configuration = new JadeConfiguration();
-        configuration.setTemplateLoader(new SparkClasspathLoader(contentRoot));
-    }
-
-    public JadeEngine(JadeConfiguration configuration) {
-        this.configuration = configuration;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     @Override
+    @SuppressWarnings("unchecked")
     public String render(ModelAndView modelAndView) {
         try {
             JadeTemplate template = configuration.getTemplate(modelAndView.getViewName());
-
-            return configuration.renderTemplate(template, (Map<String, Object>) modelAndView.getModel());
+            return configuration.renderTemplate(template,
+                    (Map<String, Object>) modelAndView.getModel());
         } catch (IOException e) {
             throw new IllegalArgumentException(e);
         }
