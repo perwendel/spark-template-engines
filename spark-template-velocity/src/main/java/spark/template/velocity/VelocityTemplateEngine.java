188a16fc68af945e1439462839f44016d73a2d89
nicosio2 <nico.gruendel@web.de>
2015-11-21 15:20:14 +0100

    Added easy to use custom encoding to VelocityTemplateEngine.

--- spark-template-velocity/src/main/java/spark/template/velocity/VelocityTemplateEngine.java
+++ spark-template-velocity/src/main/java/spark/template/velocity/VelocityTemplateEngine.java
@@ -17,6 +17,7 @@
 package spark.template.velocity;
 
 import java.io.StringWriter;
+import java.nio.charset.Charset;
 import java.util.Map;
 import java.util.Properties;
 
@@ -33,11 +34,15 @@ import spark.TemplateEngine;
 public class VelocityTemplateEngine extends TemplateEngine {
 
     private final VelocityEngine velocityEngine;
+    private String encoding;
 
     /**
      * Constructor
+     * 
+     * @param encoding The encoding the template files use. May be null.
      */
-    public VelocityTemplateEngine() {
+    public VelocityTemplateEngine(String encoding) {
+    	this.encoding = encoding;
         Properties properties = new Properties();
         properties.setProperty("resource.loader", "class");
         properties.setProperty(
@@ -45,17 +50,54 @@ public class VelocityTemplateEngine extends TemplateEngine {
                 "org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader");
         velocityEngine = new org.apache.velocity.app.VelocityEngine(properties);
     }
+    
+    /**
+     * Constructor
+     */
+    public VelocityTemplateEngine() {
+    	this((String) null);
+    }
+    
+    /**
+     * Constructor
+     * 
+     * @param charset The encoding the template files use, must not be null.
+     */
+    public VelocityTemplateEngine(Charset charset) {
+    	this(charset.name());
+    }
 
+    /**
+     * Constructor
+     *
+     * @param velocityEngine The velocity engine, must not be null.
+     * @param encoding The encoding the template files use. May be null.
+     */
+    public VelocityTemplateEngine(VelocityEngine velocityEngine, String encoding) {
+        if (velocityEngine == null) {
+            throw new IllegalArgumentException("velocityEngine must not be null");
+        }
+        this.velocityEngine = velocityEngine;
+        this.encoding = encoding;
+    }
+    
     /**
      * Constructor
      *
      * @param velocityEngine The velocity engine, must not be null.
      */
     public VelocityTemplateEngine(VelocityEngine velocityEngine) {
-        if (velocityEngine == null) {
-            throw new IllegalArgumentException("velocityEngine must not be null");
-        }
-        this.velocityEngine = velocityEngine;
+    	this(velocityEngine, (String) null);
+    }
+    
+    /**
+     * Constructor
+     *
+     * @param velocityEngine The velocity engine, must not be null.
+     * @param charset The encoding the template files use, must not be null.
+     */
+    public VelocityTemplateEngine(VelocityEngine velocityEngine, Charset charset) {
+    	this(velocityEngine, charset.name());
     }
 
     /**
@@ -63,7 +105,13 @@ public class VelocityTemplateEngine extends TemplateEngine {
      */
     @Override
     public String render(ModelAndView modelAndView) {
-        Template template = velocityEngine.getTemplate(modelAndView.getViewName());
+    	Template template;
+    	if(encoding != null || !encoding.isEmpty()) {
+    		template = velocityEngine.getTemplate(modelAndView.getViewName(), encoding);
+    	}
+    	else {
+    		template = velocityEngine.getTemplate(modelAndView.getViewName());
+    	}
         Object model = modelAndView.getModel();
         if (model instanceof Map) {
             Map<?, ?> modelMap = (Map<?, ?>) model;
