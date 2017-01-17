4c3d133d004870da3d4d267470196d234cc9e180
Victory <git@dfhu.org>
2017-01-17 14:01:11 -0600

    Rocker template (format, comment, README and LICENSE)

--- spark-template-rocker/src/main/java/spark/template/rocker/RockerTemplateEngine.java
+++ spark-template-rocker/src/main/java/spark/template/rocker/RockerTemplateEngine.java
@@ -1,20 +1,52 @@
+/*
+ * Copyright 2016 - Victory
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spark.template.rocker;
 
 import com.fizzed.rocker.RockerModel;
 import spark.ModelAndView;
 import spark.TemplateEngine;
 
+/**
+ * Render Rocker templates
+ */
 public class RockerTemplateEngine extends TemplateEngine {
 
+    /**
+     * Singleton Instance
+     */
     private static final RockerTemplateEngine INSTANCE = new RockerTemplateEngine();
 
+    /**
+     * Use getInstance() instead
+     */
     private RockerTemplateEngine() {}
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String render(ModelAndView modelAndView) {
         return ((RockerModel) modelAndView.getModel()).render().toString();
     }
 
+    /**
+     * Get an instance of the engine
+     * @return instance
+     */
     public static TemplateEngine getInstance() {
         return INSTANCE;
     }
--- spark-template-rocker/src/main/java/spark/template/rocker/RockerTemplateModel.java
+++ spark-template-rocker/src/main/java/spark/template/rocker/RockerTemplateModel.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2016 - Victory
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package spark.template.rocker;
 
 import com.fizzed.rocker.RockerModel;
@@ -6,9 +22,9 @@ import spark.ModelAndView;
 public class RockerTemplateModel extends ModelAndView {
 
     /**
-     * Constructs an instance with the template
+     * Constructs an instance with RockerModel template
      *
-     * @param template the Rocker template
+     * @param template the RockerModel template
      */
     public RockerTemplateModel(RockerModel template) {
         // No sense of template name as the template is a Rocker generated model
